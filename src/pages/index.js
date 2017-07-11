239f8b8f868ad199bfced172e5b1ef3b07fad37a
Len Smith <len@barrison.com>
2017-07-11 09:43:35 -0700

    wip

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,36 @@ import Link from 'gatsby-link'
 
 export default () => <div>
   <Link to="/talks">Talks</Link>
+  {post.node.frontmatter.title}
   Hello world!
 </div>
+
+export const pageQuery = graphql`
+  query IndexQuery {
+    site {
+      siteMetadata {
+        title
+        author
+        description
+      }
+    }
+    allMarkdownRemark(
+      limit: 2000
+      sort: { fields: [frontmatter___date], order: DESC }
+      filter: { frontmatter: { draft: { ne: true } } }
+    ) {
+      edges {
+        node {
+          fields {
+            slug
+          }
+          frontmatter {
+            title
+            date(formatString: "MMMM DD, YYYY")
+          }
+        }
+      }
+    }
+  }
+`
 
